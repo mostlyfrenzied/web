0cf88883f7a0f73322d84c63bf670ba9a1ab47b8
Subhajit Malakar <malakarsubho2004@gmail.com>
2025-05-27 02:03:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -198,7 +198,8 @@ function setupAutoRefreshControls() {
 window.onload = () => {
   getWeatherFixedLocation();
   setupAutoRefreshControls();
-  const iframe = document.querySelector(".map-embed iframe");
+}
+const iframe = document.querySelector(".map-embed iframe");
 if (iframe) {
   iframe.addEventListener("load", () => {
     iframe.classList.add("loaded");
